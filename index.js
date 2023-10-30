f8f2e675dcb651d5d6228abc219a095f85f703aa
unknown <201801083@daiict.ac.in>
2023-10-30 04:29:04 -0400

    First try

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +17,31 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+let counter = 0;
+const urlDatabase = [];
+app.post('/api/shorturl', (req, res) => {
+  const  longUrl  = req.body.url;
+  const urlPattern = /^https?:\/\/www\..+\..+/;
+
+  if (!urlPattern.test(longUrl)) {
+    return res.json({"error":"Invalid URL"});
+  }
+
+  const shortCode = counter++;
+  urlDatabase[shortCode] = longUrl;
+  const shortUrl = `${req.protocol}://${req.get('host')}/api/shorturl/${shortCode}`;
+  res.json({ shortUrl });
+});
+
+app.get('/api/shorturl/:shortCode', (req, res) => {
+  const shortCode = req.params.shortCode;
+
+  if (urlDatabase[shortCode]) {
+    res.redirect(301, urlDatabase[shortCode]);
+  } else {
+    res.status(404).json({ error: 'Short URL not found' });
+  }
 });
 
 app.listen(port, function() {
